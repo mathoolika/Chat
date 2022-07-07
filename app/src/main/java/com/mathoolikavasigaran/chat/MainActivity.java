19a61c8bc18048c75ac33500152610da9f5a9faa
mathoolika <mathoolika1412@gmail.com>
2022-07-07 11:17:51 +0200

    layout

--- app/src/main/java/com/mathoolikavasigaran/chat/MainActivity.java
+++ app/src/main/java/com/mathoolikavasigaran/chat/MainActivity.java
@@ -2,12 +2,38 @@ package com.mathoolikavasigaran.chat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button connectBtn;
+    Button sendBtn;
+    Button searchBtn;
+    ListView listView;
+    TextView message;
+    TextView status;
+    EditText space;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUI();
+    }
+
+    private void setUI() {
+        connectBtn = findViewById(R.id.connect);
+        sendBtn = findViewById(R.id.send);
+        searchBtn = findViewById(R.id.search);
+        listView = findViewById(R.id.list);
+        message = findViewById(R.id.message);
+        status = findViewById(R.id.status);
+        space = findViewById(R.id.writespace);
+
+
     }
 }
